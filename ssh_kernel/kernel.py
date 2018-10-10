74fdf1f308112646c070b646bbf896bc8b93613d
m-ueno <m-ueno@users.noreply.github.com>
2018-10-11 07:03:45 +0900

    no replwrap

--- ssh_kernel/kernel.py
+++ ssh_kernel/kernel.py
@@ -18,44 +18,6 @@ from .images import (
     extract_image_filenames, display_data_for_image, image_setup_cmd
 )
 
-class IREPLWrapper(replwrap.REPLWrapper):
-    """A subclass of REPLWrapper that gives incremental output
-    specifically for bash_kernel.
-
-    The parameters are the same as for REPLWrapper, except for one
-    extra parameter:
-
-    :param line_output_callback: a callback method to receive each batch
-      of incremental output. It takes one string parameter.
-    """
-    def __init__(self, cmd_or_spawn, orig_prompt, prompt_change,
-                 extra_init_cmd=None, line_output_callback=None):
-        self.line_output_callback = line_output_callback
-        replwrap.REPLWrapper.__init__(self, cmd_or_spawn, orig_prompt,
-                                      prompt_change, extra_init_cmd=extra_init_cmd)
-
-    def _expect_prompt(self, timeout=-1):
-        if timeout == None:
-            # "None" means we are executing code from a Jupyter cell by way of the run_command
-            # in the do_execute() code below, so do incremental output.
-            while True:
-                pos = self.child.expect_exact([self.prompt, self.continuation_prompt, u'\r\n'],
-                                              timeout=None)
-                if pos == 2:
-                    # End of line received
-                    self.line_output_callback(self.child.before + '\n')
-                else:
-                    if len(self.child.before) != 0:
-                        # prompt received, but partial line precedes it
-                        self.line_output_callback(self.child.before)
-                    break
-        else:
-            # Otherwise, use existing non-incremental code
-            pos = replwrap.REPLWrapper._expect_prompt(self, timeout=timeout)
-
-        # Prompt received, so return normally
-        return pos
-
 class SSHKernel(Kernel):
     implementation = 'ssh_kernel'
     implementation_version = __version__
