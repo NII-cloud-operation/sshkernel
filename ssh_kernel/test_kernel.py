1f4fcca9f3fada574c36b4b239e193497f5026f7
m-ueno <m-ueno@users.noreply.github.com>
2018-10-24 02:36:00 +0900

    test: paramikowrapper

--- ssh_kernel/test_kernel.py
+++ ssh_kernel/test_kernel.py
@@ -7,6 +7,7 @@ import unittest
 
 from .kernel import SSHKernel
 from .kernel import SSHWrapper
+from .kernel import SSHWrapperParamiko
 from ipykernel.kernelbase import Kernel
 
 
@@ -37,3 +38,18 @@ class SSHKernelTest(unittest.TestCase):
                 instance.process_output(stream)
 
                 instance.send_response.assert_called_once()
+
+
+class SSHWrapperParamikoTest(unittest.TestCase):
+
+    def setUp(self):
+        self.instance = SSHWrapperParamiko()
+
+    def test_exec_command_returns_error_at_first(self):
+        self.instance.exec_command('yo')
+
+    def test_exec_command_returns_stream(self):
+        self.instance._client = 0
+        self.instance.exec_command('yo')
+
+
