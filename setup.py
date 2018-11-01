7171ff8c41e3c4425e459aa77ffe8419635c5328
m-ueno <m-ueno@users.noreply.github.com>
2018-11-02 02:30:04 +0900

    tox: Remove dupulicates

--- setup.py
+++ setup.py
@@ -4,19 +4,16 @@ from ssh_kernel import __version__ as version
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def _requirements():
+    return [name.rstrip() for name in open('requirements.txt').readlines()]
+
 setuptools.setup(
     name="ssh_kernel",
     version=version,
     author="UENO, Masaru",
     author_email="m-ueno@users.noreply.github.com",
     description="SSH Kernel",
-    install_requires=[
-        'ipykernel>=5.0.0',
-        'ipython>=7.0.0',           # install.py
-        'jupyter_client>=5.2.0',    # install.py
-        'metakernel>=0.20.0',
-        'paramiko==2.4.*',
-    ],
+    install_requires=_requirements(),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
