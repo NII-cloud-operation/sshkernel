8f954be2ed2d9ff7ca5dc014de44ead54761e370
m-ueno <m-ueno@users.noreply.github.com>
2018-12-25 13:59:14 +0900

    Sort lines

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(
     description="SSH Kernel",
     extras_require={
         'dev': [
-            'pytest>=3',
             'coverage',
+            'pytest>=3',
             'tox',
         ],
     },
