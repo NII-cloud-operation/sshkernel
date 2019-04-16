498e17dd51ebee700061f7b8c57e959e5aa21776
m-ueno <m-ueno@users.noreply.github.com>
2019-04-16 14:37:42 +0900

    Add base class for exceptions

--- sshkernel/exception.py
+++ sshkernel/exception.py
@@ -1,2 +1,6 @@
-class SSHKernelNotConnectedException(Exception):
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class SSHKernelNotConnectedException(Error):
     pass
