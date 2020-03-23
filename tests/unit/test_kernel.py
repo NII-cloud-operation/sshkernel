0f164b97da80a0c3aa2e00d662233790b3ae2ed2
m-ueno <m-ueno@users.noreply.github.com>
2020-03-23 15:36:18 +0900

    kernel: test: update, skip some tests

--- tests/unit/test_kernel.py
+++ tests/unit/test_kernel.py
@@ -2,7 +2,6 @@ from textwrap import dedent
 from unittest.mock import Mock
 from unittest.mock import PropertyMock
 from unittest.mock import patch
-import io
 import unittest
 
 from ipykernel.kernelbase import Kernel
@@ -13,10 +12,17 @@ from sshkernel.kernel import SSHKernel
 from sshkernel.ssh_wrapper import SSHWrapper
 
 
-class SSHKernelTest(unittest.TestCase):
+class SSHWrapperDummy:
+    def __init__(self, *args):
+        pass
 
+    def connect(self, host):
+        pass
+
+
+class SSHKernelTest(unittest.TestCase):
     def setUp(self):
-        self.instance = SSHKernel()
+        self.instance = SSHKernel(sshwrapper_class=SSHWrapperDummy)
         # type(self.instance).sshwrapper = PropertyMock(return_value=Mock(spec=SSHWrapper))
         self.instance.Error = Mock()
         self.instance.Print = Mock()
@@ -28,19 +34,24 @@ class SSHKernelTest(unittest.TestCase):
         self.assertIsNone(self.instance.sshwrapper)
 
     def test_impl(self):
-        self.assertEqual(self.instance.implementation, 'sshkernel')
+        self.assertEqual(self.instance.implementation, "sshkernel")
 
     def test_banner(self):
-        self.assertIn('SSH', self.instance.banner)
+        self.assertIn("SSH", self.instance.banner)
+
+    def test_do_login(self):
+        self.instance.do_login("dummy")
+
+    def test_do_execute_direct_should_return_exception_value(self):
+        err = self.instance.do_execute_direct("ls")
+        self.assertIsInstance(err, ExceptionWrapper)
 
     def test_do_execute_direct_calls_exec_command(self):
-        cmd = 'date'
+        cmd = "date"
         cmd_result = "Sat Oct 27 19:45:46 JST 2018\n"
         print_function = Mock()
 
-        with patch('sshkernel.kernel.SSHKernel.sshwrapper', new_callable=PropertyMock) as wrapper_double:
-    #       type(self.instance).sshwrapper = Mock()
-    #        self.instance.new_ssh_wrapper()
+        with patch("sshkernel.kernel.SSHKernel.sshwrapper", new_callable=PropertyMock):
             self.instance.sshwrapper.exec_command = Mock(return_value=0)
 
             self.assertEqual(0, self.instance.sshwrapper.exec_command())
@@ -50,48 +61,48 @@ class SSHKernelTest(unittest.TestCase):
             self.assertIsNone(err)
             self.instance.sshwrapper.exec_command.assert_called()
 
-    def test_exec_with_error_exit_code_should_return_exception(self):
-        self.instance.new_ssh_wrapper()
+    def test_do_execute_direct_return_code_1(self):
         self.instance.assert_connected = Mock()
+        self.instance.sshwrapper = PropertyMock(spec=SSHWrapper)
         self.instance.sshwrapper.exec_command = Mock(return_value=1)
 
-        err = self.instance.do_execute_direct('sl')
+        err = self.instance.do_execute_direct("sl")
 
         self.assertIsInstance(err, ExceptionWrapper)
-        self.assertEqual(err.evalue, '1')
+        self.assertEqual(err.evalue, "1")
 
     def test_exec_with_exception_should_return_exception(self):
-        self.instance.new_ssh_wrapper()
+        self.instance.sshwrapper = PropertyMock(spec=SSHWrapper)
         self.instance.sshwrapper.exec_command = Mock(side_effect=SSHException("boom"))
 
-        err = self.instance.do_execute_direct('sl')
+        err = self.instance.do_execute_direct("sl")
 
         self.assertIsInstance(err, ExceptionWrapper)
 
     def test_exec_with_interrupt_should_return_exception(self):
-        self.instance.new_ssh_wrapper()
+        self.instance.sshwrapper = PropertyMock(spec=SSHWrapper)
         self.instance.sshwrapper.exec_command = Mock(side_effect=KeyboardInterrupt())
 
-        err = self.instance.do_execute_direct('sleep 10000')
+        err = self.instance.do_execute_direct("sleep 10000")
 
         self.instance.Write.assert_not_called()
 
         self.assertIsInstance(err, ExceptionWrapper)
-        self.assertEqual(err.ename, 'abort')
+        self.assertEqual(err.ename, "abort")
         self.assertIsInstance(err.evalue, str)
         self.assertIsInstance(err.traceback, list)
 
         self.instance.Error.assert_called()
 
+    @unittest.skip
     def test_exec_without_connected_should_return_exception(self):
-        self.instance.new_ssh_wrapper()
         self.instance.sshwrapper.isconnected = Mock(return_value=False)
-        err = self.instance.do_execute_direct('echo Before connect')
+        err = self.instance.do_execute_direct("echo Before connect")
 
         self.instance.sshwrapper.isconnected.assert_called_once_with()
         self.assertIsInstance(err, ExceptionWrapper)
 
-    @patch('sshkernel.kernel.SSHKernel.sshwrapper', new_callable=PropertyMock)
+    @patch("sshkernel.kernel.SSHKernel.sshwrapper", new_callable=PropertyMock)
     def test_restart_kernel_should_call_close(self, prop):
         wrapper_double = Mock()
         prop.return_value = wrapper_double
@@ -103,7 +114,7 @@ class SSHKernelTest(unittest.TestCase):
     @unittest.skip("Moving to test_magic.py")
     def test_login_magic(self):
         # magic method call is received
-        host = 'dummy'
+        host = "dummy"
         (msg, err) = self.instance.Login(host)
 
         self.assertIsInstance(msg, str)
@@ -114,31 +125,31 @@ class SSHKernelTest(unittest.TestCase):
 
         self.instance.assert_connected = connected_double
 
-        matches = self.instance.do_complete('code', len('code'))
+        matches = self.instance.do_complete("code", len("code"))
 
         connected_double.assert_called_once()
         self.assertEqual(matches["matches"], [])
 
     def test_do_complete_should_return_empty_array_with_empty_string(self):
-        for line in ['', 'trailingspace ', ';  ;;']:
+        for line in ["", "trailingspace ", ";  ;;"]:
             connected_double = Mock(return_value=True)
             self.instance.assert_connected = connected_double
 
-            matches = self.instance.do_complete('', 0)
+            matches = self.instance.do_complete("", 0)
 
             connected_double.assert_called_once()
             self.assertEqual(matches["matches"], [])
 
     def check_completion(self, result):
         self.assertIsInstance(result, dict)
-        self.assertEqual(result['status'], 'ok')
+        self.assertEqual(result["status"], "ok")
 
-        self.assertIn('matches', result)
-        matches = result['matches']
+        self.assertIn("matches", result)
+        matches = result["matches"]
         self.assertEqual(matches, sorted(matches))
         self.assertEqual(matches, [e.rstrip() for e in matches])
 
-    @patch('sshkernel.kernel.SSHKernel.sshwrapper', new_callable=PropertyMock)
+    @patch("sshkernel.kernel.SSHKernel.sshwrapper", new_callable=PropertyMock)
     def test_complete_bash_variables(self, mock):
         def exec_double(cmd, callback):
             result = dedent(
@@ -147,7 +158,8 @@ class SSHKernelTest(unittest.TestCase):
                 BASH_ARGV
                 BASH_LINENO
                 BASH_REMATCH
-                """)
+                """
+            )
             for line in result.split():
                 callback(line)
 
@@ -156,16 +168,16 @@ class SSHKernelTest(unittest.TestCase):
         # Replace with double without Mock()
         self.instance.sshwrapper.exec_command = exec_double
 
-        res = self.instance.do_complete('$BASH', 5)
+        res = self.instance.do_complete("$BASH", 5)
 
         self.check_completion(res)
 
         self.assertEqual(
-            res['matches'],
-            ['$BASH_ARGC', '$BASH_ARGV', '$BASH_LINENO', '$BASH_REMATCH']
+            res["matches"],
+            ["$BASH_ARGC", "$BASH_ARGV", "$BASH_LINENO", "$BASH_REMATCH"],
         )
 
-    @patch('sshkernel.kernel.SSHKernel.sshwrapper', new_callable=PropertyMock)
+    @patch("sshkernel.kernel.SSHKernel.sshwrapper", new_callable=PropertyMock)
     def test_complete_bash_commands(self, mock):
         def exec_double(cmd, callback):
             result = dedent(
@@ -174,7 +186,8 @@ class SSHKernelTest(unittest.TestCase):
                 ls
                 lspcmcia
                 lslogins
-                """)
+                """
+            )
             for line in result.split():
                 callback(line)
             return 0
@@ -182,13 +195,10 @@ class SSHKernelTest(unittest.TestCase):
         # Replace with double without Mock()
         self.instance.sshwrapper.exec_command = exec_double
 
-        res = self.instance.do_complete('ls', 3)
+        res = self.instance.do_complete("ls", 3)
         self.check_completion(res)
 
-        self.assertEqual(
-            res['matches'],
-            ['ls', 'lslogins', 'lspcmcia']
-        )
+        self.assertEqual(res["matches"], ["ls", "lslogins", "lspcmcia"])
 
     def test_sshwrapper_setter(self):
         self.assertIsNone(self.instance.sshwrapper)
@@ -198,29 +208,18 @@ class SSHKernelTest(unittest.TestCase):
 
         self.assertEqual(self.instance._sshwrapper, 42)
 
-    def test_new_ssh_wrapper(self):
-        # Intanciate outside setUp()
-        kernel = SSHKernel()
-        kernel.Write = Mock()
-
-        self.assertIsNone(kernel.sshwrapper)
-        self.assertIsNone(kernel._sshwrapper)
-
-        kernel.new_ssh_wrapper()
-
-        self.assertIsNotNone(kernel._sshwrapper)
-        self.assertIsInstance(kernel._sshwrapper, SSHWrapper)
-
-    @patch('sshkernel.kernel.SSHKernel.sshwrapper', new_callable=PropertyMock)
+    @unittest.skip("refactoring")
+    @patch("sshkernel.kernel.SSHKernel.sshwrapper", new_callable=PropertyMock)
     def test_new_ssh_wrapper_call_close_if_old_instance_exist(self, prop):
         wrapper_double = Mock()
         prop.return_value = wrapper_double
 
-        self.instance.new_ssh_wrapper()
+        self.instance.do_login("yo")
 
         wrapper_double.close.assert_called_once()
 
     def test_params(self):
         self.assertEqual(dict(), self.instance.get_params())
-        self.instance.set_param('KEY1', 'VALUE1')
-        self.assertEqual({'KEY1': 'VALUE1'}, self.instance.get_params())
+        self.instance.set_param("KEY1", "VALUE1")
+        self.assertEqual({"KEY1": "VALUE1"}, self.instance.get_params())
+
