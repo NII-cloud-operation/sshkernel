56d11c07017e71964f022431ce7460835b413f0d
m-ueno <m-ueno@users.noreply.github.com>
2020-03-30 16:22:59 +0900

    Apply black

--- tests/integration/test_sshd_integration.py
+++ tests/integration/test_sshd_integration.py
@@ -6,10 +6,9 @@ import papermill as pm
 
 
 class SSHDIntegrationTests(unittest.TestCase):
-
     def list_notebooks(self):
         here = os.path.dirname(__file__)
-        nbs = glob.glob('{}/*.ipynb'.format(here))
+        nbs = glob.glob("{}/*.ipynb".format(here))
 
         return nbs
 
@@ -18,14 +17,14 @@ class SSHDIntegrationTests(unittest.TestCase):
         nbs = self.list_notebooks()
 
         here = os.path.dirname(__file__)
-        out_dir = '{}/out'.format(here)
+        out_dir = "{}/out".format(here)
         if not os.path.exists(out_dir):
             os.mkdir(out_dir)
 
         for nb_input in nbs:
             basename = os.path.basename(nb_input)
 
-            nb_output = '{}/{}'.format(out_dir, basename)
+            nb_output = "{}/{}".format(out_dir, basename)
 
             try:
                 pm.execute_notebook(nb_input, nb_output)
